513f3ccba810201815d805365248a9386206f300
Rish678 <rishith.athikam@gmail.com>
2021-02-21 18:15:07 -0500

    uploading project

---
+++ random password generator/assets/js/main.js
@@ -0,0 +1,67 @@
+
+//generate random password
+function generate() {
+
+    //set password length/complexity
+    let complexity = document.getElementById("slider").value;
+
+    //possible password values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    let password = "";
+
+    //create for loop to choose password characters
+    for (var i = 0; i <= complexity; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+
+    //add password to textbox/display area
+    document.getElementById("display").value = password;
+
+    //add password to previously generated passwords section
+    document.getElementById("lastNums").innerHTML += password + "<br />";
+
+}
+
+//set default length display of 25
+document.getElementById("length").innerHTML = "Length: 25";
+
+//function to set length based on slider position
+document.getElementById("slider").oninput = function () {
+
+    if (document.getElementById("slider").value > 0) {
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else {
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+
+}
+
+//function to copy password to clipboard
+function copyPassword() {
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied to clipboard!");
+
+}
+
+function generateTxtFile() {
+    var text = "Password: " + document.getElementById("display").value;
+    console.log(text);
+    downloadToFile(text, 'myPassword.txt', 'text/plain');
+}
+
+const downloadToFile = (content, filename, contentType) => {
+    const a = document.createElement('a');
+    const file = new Blob([content], { type: contentType });
+
+    a.href = URL.createObjectURL(file);
+    a.download = filename;
+    a.click();
+
+    URL.revokeObjectURL(a.href);
+};
